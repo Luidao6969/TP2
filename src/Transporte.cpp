f12124295a375985f69f85b2373582950ea35292
Luidao6969 <deivijones24@gmail.com>
2025-06-18 14:02:45 -0300

    Update Transporte.cpp

--- src/Transporte.cpp
+++ src/Transporte.cpp
@@ -1,5 +1,6 @@
 #include "../include/Transporte.h"
 #include <iostream>
+using namespace std;
 
 Transporte::Transporte(int origem, int destino, int capacidade)
     : origem(origem), destino(destino), capacidade(capacidade), count(0) {
@@ -11,18 +12,48 @@ Transporte::~Transporte() {
 }
 
 void Transporte::carregarPacotes(Armazem* armazemOrigem) {
+    if (!armazemOrigem) {
+        cerr << "Armazém de origem inválido!" << endl;
+        return;
+    }
+
+    StackPacote& stack = armazemOrigem->getStack(destino);
+    
     for (int i = 0; i < capacidade; ++i) {
-        Pacote* p = armazemOrigem->getStack(destino).pop();
-        if (!p) break;
-        pacotes[count++] = p;
-        std::cout << "Transportando pacote " << p->getId() << " de " << origem << " para " << destino << "\n";
+        if (stack.vazia()) break;
+        
+        Pacote* p = stack.pop();
+        if (p) {
+            if (count < capacidade) {
+                pacotes[count++] = p;
+                cout << "Carregado pacote " << p->getId() << endl;
+            } else {
+                cerr << "Capacidade excedida!" << endl;
+                stack.push(p); // Devolve o pacote se não couber
+                break;
+            }
+        }
     }
 }
 
 void Transporte::descarregarPacotes(Armazem* armazemDestino) {
-    for (int i = 0; i < count; ++i) {
-        pacotes[i]->avancarNaRota(); // método para mudar origem interna
-        armazemDestino->receberPacote(pacotes[i]);
+    if (!armazemDestino) {
+        cerr << "Armazém de destino inválido!" << endl;
+        return;
     }
-    count = 0;
+
+    for (int i = 0; i < count; ++i) {
+        Pacote* p = pacotes[i];
+        if (p) {
+            int proximoSalto = p->getProximoPasso();
+            if (proximoSalto >= 0) {
+                armazemDestino->getStack(proximoSalto).push(p);
+                cout << "Descarregado pacote " << p->getId() << " em " << proximoSalto << endl;
+            } else {
+                cout << "Pacote " << p->getId() << " chegou ao destino final" << endl;
+                delete p; // Ou outro tratamento para pacotes entregues
+            }
+        }
+    }
+    count = 0; // Reseta o contador após descarregar
 }
